6ec9ffdfddc84456013340ab6a46aef88e40ad60
Michael <michaellll11@abv.bg>
2024-07-02 17:41:45 +0300

    Start the project

---
+++ src/main/java/bg/softuni/cardealer/model/User.java
@@ -0,0 +1,12 @@
+package bg.softuni.cardealer.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity{
+
+
+}
---
+++ src/main/java/bg/softuni/cardealer/web/HomeController.java
@@ -0,0 +1,15 @@
+package bg.softuni.cardealer.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+}
