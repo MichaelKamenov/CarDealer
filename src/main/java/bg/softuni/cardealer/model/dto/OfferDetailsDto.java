720fc468d626c8f85cfd8936dd28e5434e9e874b
Michael <michaellll11@abv.bg>
2024-07-11 20:32:51 +0300

    Offer Details

---
+++ src/main/java/bg/softuni/cardealer/model/dto/OfferDetailsDto.java
@@ -0,0 +1,6 @@
+package bg.softuni.cardealer.model.dto;
+
+import bg.softuni.cardealer.model.enums.EngineType;
+
+public record OfferDetailsDto(Long id, String description, Integer mileage, EngineType engineType) {
+}
