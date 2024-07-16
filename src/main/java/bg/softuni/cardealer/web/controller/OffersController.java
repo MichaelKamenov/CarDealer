cc07ef5c24005ec6f92e38c16b96ccd3cbabf365
Michael <michaellll11@abv.bg>
2024-07-16 19:35:37 +0300

    Offers controller and offersDto

---
+++ src/main/java/bg/softuni/cardealer/web/controller/OffersController.java
@@ -0,0 +1,27 @@
+package bg.softuni.cardealer.web.controller;
+
+import bg.softuni.cardealer.service.OfferService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("offers")
+public class OffersController {
+
+    private final OfferService offerService;
+
+    public OffersController(OfferService offerService) {
+        this.offerService = offerService;
+    }
+
+    @GetMapping("/all")
+    public String getAllOffers(Model model){
+        model.addAttribute("allOffers", offerService.getAllOffers());
+        return "offers";
+    }
+
+
+}
